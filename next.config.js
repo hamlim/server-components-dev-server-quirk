677643b3a4aa5b5312948b2a77131fb1873d64aa
Matt Hamlin <matthewjameshamlin@gmail.com>
2022-07-22 06:36:53 -0400

    Server components in pages/ dir

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    serverComponents: true,
+    runtime: "nodejs",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
